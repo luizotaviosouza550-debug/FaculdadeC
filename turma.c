1c72b58eaffdd11cec13de39e8183a49b27745b5
luizotaviosouza550-debug <luizotaviosouza550@gmail.com>
2025-08-28 14:48:46 -0300

    Create turma.c

---
+++ turma.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int idade,matricula;
+    float altura ;
+    char nome[50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+
+    printf("Digite seu nome: \n");
+    scanf("%s", nome);
+
+    printf("Digite sua matrícula: \n");
+    scanf("%d",&matricula);
+
+    printf("\n--- Dados do Aluno ---\n");
+    printf("Nome do aluno: %s - Matrícula: %d\n",nome,matricula);
+    printf("idade: %d - altura: %.2f\n",idade,altura);
+
+    return 0;
+}
